1869f4cc989e19dfade58bfe7c9eb463f62a9097
=Xavier <github@tttp.eu>
2023-02-10 06:35:31 +0100

    add more details on log

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const geoname =require ('./src/download.js');
-const csv = require ('./src/jsonify.js');
+const geoname = require("./src/download.js");
+const csv = require("./src/jsonify.js");
 
 var args = process.argv.slice(2);
 
-if (!args[0]) 
+if (!args[0])
   process.exit(console.error("syntax: $node index.js {COUNTRY_ISO}"));
-console.log('country: ', args[0]);
+console.log("country: ", args[0]);
 
-const run = async (country) =>{
-let r= await geoname.download (country);
-console.log("download",r);
-r = await geoname.unzip (country);
-console.log("unzip",r);
-r= await csv.jsonify (country);
-console.log("jsonify",r);
+const run = async (country) => {
+  let r = await geoname.download(country);
+  console.log("download", r);
+  r = await geoname.unzip(country);
+  console.log("unzip", r);
+  r = await csv.jsonify(country);
+  console.log("jsonify", r);
+  console.log(
+    "todo: node src/workerify.js " +
+      country +
+      ";cd wrangler;wrangler publish -e " +
+      country
+  );
 };
 
-console.log ("todo: node src/workerify.js "+ country ";cd wrangler;wrangler publish -e "+country);
 run(args[0]);
